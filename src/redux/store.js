5c2e9ba4c80461da8d6b834854ffb947e812c395
Kirsi Jokela <kirsijo@gmail.com>
2022-09-04 20:15:54 +0300

    index.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import toDoReducer from './todoSlice';
 
 export default configureStore({
- reducer: {// allows you create n number of sliders
+ reducer: {
      toDo: toDoReducer
  },
 })
